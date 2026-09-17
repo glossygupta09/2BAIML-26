c5c1aa6490f70f829a2c25fc3c068fe989e6ffba
Glossy Gupta <glossygupta@gmail.com>
2026-09-17 12:23:38 +0530

    exp3

--- Exp3.js
+++ Exp3.js
@@ -25,7 +25,7 @@ class Manager extends Employee{
     }
 }
 const emp1 = new Employee(101,"Glossy",50000);
-const emp2 = new Employee(102,"Trisha",60000);
+const emp2 = new Employee(102,"Trishaa",60000);
 
 const mgr1 = new Manager(201,"Rahul",70000,10000);
 emp1.displaySalary();
